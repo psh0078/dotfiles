eb8cfd593517f796a3f85457e5bb1dfd790e58b7
psh0078 <nws0078@gmail.com>
2025-05-02 14:11:43 -0400

    change nvim theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -15,6 +15,7 @@ return {
       -- Optionally configure and load the colorscheme
       -- directly inside the plugin declaration.
       vim.g.sonokai_enable_italic = true
+      -- vim.g.sonokai_style = "atlantis"
       vim.cmd.colorscheme('sonokai')
     end
   }
